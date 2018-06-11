4af04fbde7d12544ae1a1c24d27b5391c806bce5
Sharat Sachin <sharat.ss.sachin@gmail.com>
2018-06-11 07:52:13 +0530

    Update 386B.cpp

--- CodeForces/386B.cpp
+++ CodeForces/386B.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 #define FR(i,a,b) for(auto i=(a);i<(b);i++)
 #define FV(i,v) for(auto i=(v).begin();i!=(v).end();++i)
-#define ALL(v) ((v).begin(), (v).end())
+#define ALL(v) ((v).begin(), (v).end()) 
 
 using ll = long long;
 
