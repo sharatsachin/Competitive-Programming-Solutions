c98d5c77504f77e1d42b9350ad5162614bb45205
Sharat Sachin <sharat.ss.sachin@gmail.com>
2018-06-12 17:58:07 +0530

    Create 360B.cpp

---
+++ CodeForces/360B.cpp
@@ -0,0 +1,18 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+#define FR(i,a,b) for(auto i=(a);i<(b);i++)
+#define FV(i,v) for(auto i=(v).begin();i!=(v).end();++i)
+#define ALL(v) ((v).begin(), (v).end())
+
+using ll = long long;
+
+int main(){
+	ios_base::sync_with_stdio(false);cin.tie(0);
+	string n;
+	cin>>n;
+	cout<<n;
+	reverse(n.begin(), n.end());
+	cout<<n;
+	return 0;
+}
